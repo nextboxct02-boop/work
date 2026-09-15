77b7042cca43c74fdcb069492c0e4179a8286f97
nextboxct02-boop <nextboxct02@gmail.com>
2026-09-15 11:28:00 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,16 @@
+// ไฟล์นี้ใส่ค่า Public configuration เท่านั้น
+// OAuth Client ID เป็น public identifier ได้ แต่ห้ามใส่ Client Secret ลงในเว็บ/GitHub
+
 window.APP_CONFIG = {
   GOOGLE_CLIENT_ID: "31070724397-dc1j1e59s6p6o29jm8kn6324qracf1d9.apps.googleusercontent.com",
+
   SPREADSHEET_ID: "16xvGG79nem8PeHA2ED2G_-8ENEP7KMzq0Mp4zKelGT8",
+
   SHEET_NAME: "อัพเดตลูกค้า",
+
   SHEET_GID: "91690945",
+
   DATA_START_ROW: 6,
+
   ALLOWED_EMAILS: ["nextboxct02@gmail.com"],
 };
